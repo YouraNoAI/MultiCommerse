86cabdd149030e2e17e838ad918db21272f399fb
YouraNoAI <dhoyoxofficial23@gmail.com>
2024-12-12 23:09:13 +0700

    Changing themes for Checkout Pages

--- Pages/Checkout.jsx
+++ Pages/Checkout.jsx
@@ -1,9 +1,10 @@
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import logo from "../src/assets/logo.png";
 
 const Checkout = () => {
-  const img = "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg";
+  const { id } = useParams();
 
   const [paymentMethod, setPaymentMethod] = useState("");
   const [address, setAddress] = useState("");
@@ -18,7 +19,9 @@ const Checkout = () => {
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const response = await axios.get(`/api/products`);
+      const response = await axios.get(
+        `https://fakestoreapi.com/products/${id}`
+      );
       setProducts(response.data);
     };
     fetchProducts();
@@ -36,114 +39,170 @@ const Checkout = () => {
     alert(phone);
   };
 
-  const qty = quantity;
+  const shipping = 0.5;
+  const quantityBuy = products.price + shipping;
 
+  const handlePaymentMethod = (method) => {
+    setPaymentMethod(method);
+    document.querySelector(".paypal-button").classList.add("active");
+    document.querySelector(".credit-card-button").classList.remove("active");
+    document.querySelector(".debit-card-button").classList.remove("active");
+  };
 
   return (
     <div className="checkout-products">
-      <div className="checkout-products-content">
-        <div className="preview-checkout-box">
-          <img src={img} alt="product" />
-          <div className="preview-checkout-box-content">
-            <h1>Product Name</h1>
-            <p>Product Price</p>
-            <div className="button-qty">
-              <button onClick={() => setQuantity(quantity - 1)}>
-                {" "}
-                <i className="fa fa-minus"></i>
-              </button>
-              <input
-                type="text"
-                value={quantity}
-                onChange={(e) => setQuantity(e.target.value)}
-              />
-              <h3>{qty}</h3>
-              <button onClick={() => setQuantity(quantity + 1)}>
-                {" "}
-                <i className="fa fa-plus"></i>
-              </button>
+      <div className="checkout-products-header">
+        <div className="checkout-products-header-left">
+          <img src={logo} alt="logo" />
+          <h1>Multi Commerce</h1>
+        </div>
+      </div>
+      <div className="checkout-products-body">
+        <div className="checkout-products-body-left">
+          <div className="checkout-products-body-left-box">
+            <h1
+              style={{
+                textAlign: "center",
+                marginTop: "25px",
+                fontSize: "42px",
+                fontWeight: "900",
+                fontFamily: "Crimson Pro",
+              }}
+            >
+              Checkout
+            </h1>
+            <div className="checkout-products-body-left-box-shipping">
+              <div className="shipping-method active">
+                <h1>Delivery</h1>
+              </div>
+              <div className="shipping-method">
+                <h1>Standard</h1>
+              </div>
+              <div className="shipping-method">
+                <h1>Pickup</h1>
+              </div>
             </div>
+            <form onSubmit={handleSubmit} style={{ marginTop: "25px" }}>
+              <div className="name">
+                <label htmlFor="name">Name</label>
+                <input
+                  type="text"
+                  placeholder="Name"
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="email">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="text"
+                  placeholder="Email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="phone">
+                <label htmlFor="phone">Phone</label>
+                <input
+                  type="text"
+                  placeholder="Phone"
+                  onChange={(e) => setPhone(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="address-container">
+                <div className="address">
+                  <label htmlFor="address">Address</label>
+                  <input
+                    type="text"
+                    placeholder="Address"
+                    onChange={(e) => setAddress(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="city">
+                  <label htmlFor="city">City</label>
+                  <input
+                    type="text"
+                    placeholder="City"
+                    onChange={(e) => setCity(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="state">
+                  <label htmlFor="state">State</label>
+                  <input
+                    type="text"
+                    placeholder="State"
+                    onChange={(e) => setState(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="zip">
+                  <label htmlFor="zip">Zip</label>
+                  <input
+                    type="text"
+                    placeholder="Zip"
+                    onChange={(e) => setZip(e.target.value)}
+                    required
+                  />
+                </div>
+              </div>
+              <div className="paymentMethod">
+                <div
+                  className="payPal"
+                  onClick={() => handlePaymentMethod("PayPal")}
+                >
+                  <h1 className="active">PayPal</h1>
+                </div>
+                <div
+                  className="creditCard"
+                  onClick={() => handlePaymentMethod("Credit Card")}
+                >
+                  <h1>Credit Card</h1>
+                </div>
+                <div
+                  className="debitCard"
+                  onClick={() => handlePaymentMethod("Debit Card")}
+                >
+                  <h1>Debit Card</h1>
+                </div>
+              </div>
+            </form>
           </div>
         </div>
-        <form className="form-checkout-box" onSubmit={handleSubmit}>
-          <div className="address-checkout-box">
-            <h1>Address</h1>
-            <input
-              type="text"
-              placeholder="Name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-            <input
-              type="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <input
-              type="text"
-              placeholder="Phone"
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
-            />
-            <input
-              type="text"
-              placeholder="Address"
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
-            />
-            <input
-              type="text"
-              placeholder="City"
-              value={city}
-              onChange={(e) => setCity(e.target.value)}
-            />
-            <input
-              type="text"
-              placeholder="State"
-              value={state}
-              onChange={(e) => setState(e.target.value)}
-            />
-            <input
-              type="number"
-              placeholder="Zip"
-              value={zip}
-              onChange={(e) => setZip(e.target.value)}
-            />
-          </div>
-          <div className="payment-checkout-box">
-            <h1>Payment Method</h1>
-            <div className="payment-method-checkout-box">
-              <div
-                className="paypal-button active"
-                onClick={() => setPaymentMethod("paypal")}
-              >
-                <p>
-                  <i className="fa fa-cc-paypal"></i> PayPal
-                </p>
-              </div>
-              <div
-                className="credit-card-button"
-                onClick={() => setPaymentMethod("credit-card")}
-              >
-                <p>
-                  <i className="fa fa-credit-card"></i> Credit Card
-                </p>
-              </div>
-              <div
-                className="debit-card-button"
-                onClick={() => setPaymentMethod("debit-card")}
-              >
-                <p>
-                  <i className="fa fa-credit-card"></i> Debit Card
-                </p>
-              </div>
+        <div className="checkout-products-body-right">
+          <h1>Review Your Order</h1>
+          <div className="checkout-products-body-right-box">
+            <div className="checkout-products-body-right-box-left">
+              <img src={products.image} alt="product" />
             </div>
-            <button type="submit" onClick={handleSubmit}>
-              Submit
-            </button>
+            <div className="checkout-products-body-right-box-right">
+              <p>{products.title}</p>
+            </div>
           </div>
-        </form>
+          <div className="checkout-products-body-right-box-right-table">
+            <table>
+              <tr>
+                <td>Quantity</td>
+                <td>: {quantity}</td>
+              </tr>
+              <tr>
+                <td>Shipping</td>
+                <td>: {shipping}</td>
+              </tr>
+              <tr>
+                <td>Subtotal</td>
+                <td>: {quantityBuy}</td>
+              </tr>
+              <tr>
+                <td>Total</td>
+                <td>: {quantityBuy + shipping}</td>
+              </tr>
+            </table>
+          </div>
+          <button type="submit">Place Order</button>
+        </div>
       </div>
     </div>
   );
